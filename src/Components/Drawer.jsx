32bffaa7f6f7cb67246feaec394e2928d2b97b1b
karim742 <121242355+karim742@users.noreply.github.com>
2024-01-05 23:57:07 +0600

    tailwind classname organized

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -1,74 +1,74 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Drawer = () => {
   const [showDrawer, setShowDrawer] = useState(false);
   return (
-    <header className='bg-[#f7f7f7] shadow-headerShadow'>
+    <header className="bg-[#f7f7f7] shadow-headerShadow">
       {/* container  */}
-      <div className='max-w-screen-2xl mx-auto p-2'>
+      <div className="mx-auto max-w-screen-2xl p-2">
         {/* wrapper  */}
-        <div className='flex justify-between items-center'>
+        <div className="flex items-center justify-between">
           {/* logo  */}
           <div>
-            <img src='https://cutt.ly/XwHkvO0d' alt='logo' className='w-24' />
+            <img src="https://cutt.ly/XwHkvO0d" alt="logo" className="w-24" />
           </div>
           {/* Drawer toggle icon  */}
           <div
-            className='block md:hidden'
+            className="block md:hidden"
             onClick={() => setShowDrawer(!showDrawer)}
           >
             {showDrawer ? (
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                version='1.1'
-                xmlnsXlink='http://www.w3.org/1999/xlink'
-                width='32'
-                height='32'
-                x='0'
-                y='0'
-                viewBox='0 0 320.591 320.591'
-                style={{ enableBackground: 'new 0 0 512 512' }}
-                xmlSpace='preserve'
-                className=''
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="32"
+                height="32"
+                x="0"
+                y="0"
+                viewBox="0 0 320.591 320.591"
+                style={{ enableBackground: "new 0 0 512 512" }}
+                xmlSpace="preserve"
+                className=""
               >
                 <g>
                   <path
-                    d='M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z'
-                    fill='#000000'
-                    opacity='1'
-                    data-original='#000000'
-                    className=''
+                    d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                    fill="#000000"
+                    opacity="1"
+                    data-original="#000000"
+                    className=""
                   ></path>
                   <path
-                    d='M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z'
-                    fill='#000000'
-                    opacity='1'
-                    data-original='#000000'
-                    className=''
+                    d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                    fill="#000000"
+                    opacity="1"
+                    data-original="#000000"
+                    className=""
                   ></path>
                 </g>
               </svg>
             ) : (
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                version='1.1'
-                xmlnsXlink='http://www.w3.org/1999/xlink'
-                width='32'
-                height='32'
-                x='0'
-                y='0'
-                viewBox='0 0 124 124'
-                style={{ enableBackground: 'new 0 0 512 512' }}
-                xmlSpace='preserve'
-                className=''
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="32"
+                height="32"
+                x="0"
+                y="0"
+                viewBox="0 0 124 124"
+                style={{ enableBackground: "new 0 0 512 512" }}
+                xmlSpace="preserve"
+                className=""
               >
                 <g>
                   <path
-                    d='M112 6H12C5.4 6 0 11.4 0 18s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 94H12c-6.6 0-12 5.4-12 12s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z'
-                    fill='#000000'
-                    opacity='1'
-                    data-original='#000000'
-                    className=''
+                    d="M112 6H12C5.4 6 0 11.4 0 18s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 94H12c-6.6 0-12 5.4-12 12s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
+                    fill="#000000"
+                    opacity="1"
+                    data-original="#000000"
+                    className=""
                   ></path>
                 </g>
               </svg>
@@ -76,23 +76,23 @@ const Drawer = () => {
           </div>
           {/* nav Item  */}
           <nav
-            className={`h-screen md:h-auto flex items-center gap-5 md:gap-10 absolute md:static flex-col md:flex-row top-28 ${
-              showDrawer ? 'left-0' : '-left-40'
-            } bg-[#f7f7f7] md:bg-none transition-all z-50`}
+            className={`absolute top-28 flex h-screen flex-col items-center gap-5 md:static md:h-auto md:flex-row md:gap-10 ${
+              showDrawer ? "left-0" : "-left-40"
+            } z-50 bg-[#f7f7f7] transition-all md:bg-none`}
           >
-            <a href='#' className='text-xl font-medium px-5 py-1 md:p-0'>
+            <a href="#" className="px-5 py-1 text-xl font-medium md:p-0">
               Home
             </a>
-            <a href='#' className='text-xl font-medium px-5 py-1 md:p-0'>
+            <a href="#" className="px-5 py-1 text-xl font-medium md:p-0">
               Blog
             </a>
-            <a href='#' className='text-xl font-medium px-5 py-1 md:p-0'>
+            <a href="#" className="px-5 py-1 text-xl font-medium md:p-0">
               Contact
             </a>
-            <a href='#' className='text-xl font-medium px-5 py-1 md:p-0'>
+            <a href="#" className="px-5 py-1 text-xl font-medium md:p-0">
               Portfolio
             </a>
-            <a href='#' className='text-xl font-medium px-5 py-1 md:p-0'>
+            <a href="#" className="px-5 py-1 text-xl font-medium md:p-0">
               Login
             </a>
           </nav>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 const slidesArray = [
   {
-    image: 'https://cutt.ly/DwHkHrdg',
+    image: "https://cutt.ly/DwHkHrdg",
   },
   {
-    image: 'https://cutt.ly/9wHkJyvI',
+    image: "https://cutt.ly/9wHkJyvI",
   },
   {
-    image: 'https://cutt.ly/DwHkHrdg',
+    image: "https://cutt.ly/DwHkHrdg",
   },
   {
-    image: 'https://cutt.ly/9wHkJyvI',
+    image: "https://cutt.ly/9wHkJyvI",
   },
 ];
 const Slider = () => {
   const [counter, setCounter] = useState(0);
   const length = slidesArray?.length;
   useEffect(() => {
-    const slidesNode = document.querySelectorAll('.slide');
+    const slidesNode = document.querySelectorAll(".slide");
     slidesNode.forEach((slide, index) => {
       slide.style.left = `${index * 100}%`;
     });
@@ -28,30 +28,30 @@ const Slider = () => {
   return (
     <section>
       {/* slides wrapper  */}
-      <div className='w-full h-60 md:h-96 lg:h-[36rem] relative overflow-hidden'>
+      <div className="relative h-60 w-full overflow-hidden md:h-96 lg:h-[36rem]">
         {slidesArray.map((singleSlide, index) => (
           <div
             key={index}
-            className='slide w-full h-full absolute top-0 transition-all'
+            className="slide absolute top-0 h-full w-full transition-all"
           >
             <img
               src={singleSlide.image}
-              alt='slide image'
-              className='w-full object-cover'
+              alt="slide image"
+              className="w-full object-cover"
             />
           </div>
         ))}
         {/* controller  */}
-        <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-between w-full px-5'>
+        <div className="absolute left-[50%] top-[50%] flex w-full translate-x-[-50%] translate-y-[-50%] justify-between px-5">
           <span
             onClick={() => setCounter(counter < length ? counter - 1 : length)}
-            id='prev'
-            className='w-16 h-16 flex justify-center items-center text-3xl font-black bg-slate-300/50 text-black rounded-full cursor-pointer'
+            id="prev"
+            className="flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-slate-300/50 text-3xl font-black text-black"
           >{` < `}</span>
           <span
             onClick={() => setCounter(counter > length ? 0 : counter + 1)}
-            id='next'
-            className='w-16 h-16 flex justify-center items-center text-3xl font-black bg-slate-300/50 text-black rounded-full cursor-pointer'
+            id="next"
+            className="flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-slate-300/50 text-3xl font-black text-black"
           >{` > `}</span>
         </div>
       </div>
